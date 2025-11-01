e7f45a0aede67528db3d2cbccc19775fa1222238
Timothy Murphy <tnmurphy@gmail.com>
2025-11-01 09:28:41 +0000

    missed out strcache.h

---
+++ src/strcache.h
@@ -0,0 +1,22 @@
+/* Constant string caching for GNU Make.
+Copyright (C) 2006-2024 Free Software Foundation, Inc.
+This file is part of GNU Make.
+
+GNU Make is free software; you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
+
+GNU Make is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+#ifndef _STRCACHE_H_
+#define _STRCACHE_H_
+
+extern struct hash_table strings;
+
+#endif
